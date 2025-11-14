733e34fef4435fab0c9328a8a67e4e767dc9ed30
robert bolkan <robert.bolkan@yahoo.com>
2025-11-14 15:17:48 -0800

    Add mod1.py and image.PNG

---
+++ mod1/mod1.py
@@ -0,0 +1,63 @@
+# Part 1 – Addition and Subtraction Pseudocode
+
+# Start
+
+# Display: “Enter the first number:”
+
+# Get num1 from the user
+
+# Display: “Enter the second number:”
+
+# Get num2 from the user
+
+# Set addition_result = num1 + num2
+
+# Set subtraction_result = num1 - num2
+
+# Display “Addition result:” and addition_result
+
+# Display “Subtraction result:” and subtraction_result
+
+# End
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+addition_result = num1 + num2
+subtraction_result = num1 - num2
+
+print("Addition result:", addition_result)
+print("Subtraction result:", subtraction_result)
+
+
+# Part 2 – Multiplication and Division Pseudocode
+# Start
+
+# Display: “Enter the first number:”
+
+# Get num1 from the user
+
+# Display: “Enter the second number:”
+
+# Get num2 from the user
+
+# Set multiplication_result = num1 * num2
+
+# Set division_result = num1 / num2
+
+# Display “Multiplication result:” and multiplication_result
+
+# End
+
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+multiplication_result = num1 * num2
+division_result = num1 / num2
+
+print("Multiplication result:", multiplication_result)
+print("Division result:", division_result)
+
+
+
